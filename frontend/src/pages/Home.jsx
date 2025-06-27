0e22d2121982dcb38800f0952c84ee5a413bb6f0
ramees7 <muhammedramees09876@gmail.com>
2025-06-27 17:16:24 +0530

    home page design almost completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <GenderCollectionSection />
+      <NewArrivals />
+      {/* Best Seller */}
+      <h2 className="text-3xl font-bold mb-4 text-center">Best Seller</h2>
+      <ProductDetails />
     </div>
   );
 };
